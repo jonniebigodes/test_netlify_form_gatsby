50c5a6a94251b18343deeaa2388d960523febb34
jonniebigodes <jonniebigodes@hotmail.com>
2019-04-07 21:11:48 +0100

    moved into contact page the form

---
+++ src/pages/contact.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import DummyForm from "../components/dummyForm"
+
+export default ()=><div><DummyForm/></div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react"
-import DummyForm from "../components/dummyForm"
+
 export default () => (
   <div>
     <h2>Send me a line</h2>
-    <DummyForm/>
+    
   </div>
 )
